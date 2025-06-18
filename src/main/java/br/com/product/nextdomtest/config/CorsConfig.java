f7054ee28b4ea9e3cec998713c54c20d9f6f5a5f
jardelrodriguespinto <ghmagno910@gmail.com>
2025-06-18 10:42:20 -0300

    alterando cors para incluir rota da aplicação na Vercel

--- src/main/java/br/com/product/nextdomtest/config/CorsConfig.java
+++ src/main/java/br/com/product/nextdomtest/config/CorsConfig.java
@@ -12,7 +12,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                .allowedOrigins("http://localhost:5173",
+                                "https://nextdomfrontend.vercel.app/",
+                                "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
